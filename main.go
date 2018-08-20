0aa451c284ca797845e7615ee61f9f0debd7bece
Joseph Sungpil Choi <chousemath@gmail.com>
2018-08-20 21:44:17 +0900

    Add string reversal

--- main.go
+++ main.go
@@ -8,8 +8,25 @@ import (
 	"time"
 )
 
-func say(name, message string) {
-	fmt.Printf("\n[%s]\n$%s: %s\n\n", time.Now().Format("2006-01-02 15:04:05"), name, message)
+type message struct {
+	Name      string
+	Content   string
+	Timestamp string
+}
+
+func now() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+func say(msg message) {
+	fmt.Printf("\n\t[%s]\n\t$%s: %s\n\n", msg.Timestamp, msg.Name, msg.Content)
+}
+func reverse(msgContent string) string {
+	msgContent = strings.TrimSuffix(strings.Replace(msgContent, "$reverse", "", -1), " ")
+	runes := []rune(msgContent)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
 func main() {
@@ -18,7 +35,24 @@ func main() {
 	name, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	name = strings.TrimSuffix(name, "\n")
-	say(name, "I have entered the chatroom!")
+	msg := message{name, "I have entered the chatroom!", now()}
+	say(msg)
+
+	for true {
+		msgContent, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		msg = message{name, strings.TrimSuffix(msgContent, "\n"), now()}
+		if msg.Content == "exit" {
+			break
+		} else if strings.Contains(msg.Content, "$reverse") {
+			msg.Content = reverse(msg.Content)
+		}
+		say(msg)
+	}
 }
