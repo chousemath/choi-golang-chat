5a7c4805d15e6a32954f8e36d0af156edbf3204c
Joseph Sungpil Choi <chousemath@gmail.com>
2018-08-20 21:12:31 +0900

    Add name input

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
-func say(s string) {
-	fmt.Print("\n", time.Now(), "\n >>> ", s, "\n\n")
+func say(name, message string) {
+	fmt.Printf("\n[%s]\n$%s: %s\n\n", time.Now().Format("2006-01-02 15:04:05"), name, message)
 }
 
 func main() {
-	say("Welcome to Choi-Golang-Chat~")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What is your name?: ")
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	name = strings.TrimSuffix(name, "\n")
+	say(name, "I have entered the chatroom!")
 }
